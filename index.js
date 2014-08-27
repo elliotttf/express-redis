d570e054f66f27db6ffc3c0a8120af2805fe25d2
Elliott Foster <elliottf@codebrews.com>
2014-08-27 14:05:41 -0500

    Add repo and debug options.

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-'use strict'
+/**
+ * Express - Redis
+ * Copyright(c) 2014 Elliott Foster <elliottf@codebrews.com>
+ * MIT Licensed.
+ */
 
 var redis = require('redis');
+var debug = require('debug')('express:redis');
 
 module.exports = function (port, host, options) {
   port = port || 6379;
@@ -10,7 +15,8 @@ module.exports = function (port, host, options) {
   var client = redis.createClient(port, host, options);
 
   var f = function (req, res, next) {
-    client.on('connect', function () {
+    client.on('ready', function () {
+      debug('Redis connection ready.');
       req.db = client;
       next();
     });
