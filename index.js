4c3907780981817c0a4220d07703b8fdd1972115
Elliott Foster <elliottf@codebrews.com>
2014-08-27 13:27:30 -0500

    Initial commit.

---
+++ index.js
@@ -0,0 +1,21 @@
+'use strict'
+
+var redis = require('redis');
+
+module.exports = function (redis, port, host, options) {
+  port = port || 6379;
+  host = host || '127.0.0.1';
+  options = options || {};
+
+  var client = redis.createClient(port, host, options);
+
+  var f = function (req, res, next) {
+    client.on('connect', function () {
+      req.db = client;
+      next();
+    });
+  };
+
+  return f;
+};
+
